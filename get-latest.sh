3155b9e42d50450e68c0c558bec1679d403f40f3
shanmdphd <pil.han@gmail.com>
2021-03-23 23:51:52 +0900

    update: go

---
+++ get-latest.sh
@@ -0,0 +1,8 @@
+# https://github.com/wowchemy/wowchemy-hugo-modules
+
+hugo mod clean
+
+# 2021-03-22
+hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@17505c4581cd43210579b26dd3e01952ce9682d8
+hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@17505c4581cd43210579b26dd3e01952ce9682d8
+
